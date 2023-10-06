b6e15e8cd2e8aa89c65e1b790a695567e1d4132c
EJ-RYU <ejthebest4ever@nate.com>
2023-10-06 13:13:32 +0900

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by EJ on 2023/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
